c083487a0451bc2b86c5bcc0a979290001af5475
Varad-ctrl <varadjadhav3663@gmail.com>
2026-07-23 19:03:42 +0530

    Add Prometheus metrics endpoint

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,6 @@ const app = require('./app');
 const prisma = require('./config/prisma');
 const logger = require('./config/logger');
 const { startCronJobs } = require('./config/cron');
-const { register } = require("./monitoring/metrics");
 const PORT = process.env.PORT || 5000;
 
 const start = async () => {
